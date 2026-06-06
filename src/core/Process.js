6f604d1b0362c7b0d0f648647f2191bbe0934d0c
lovell <xuansontrannguyen06@gmail.com>
2026-06-06 16:42:21 +0700

    add Process class

--- src/core/Process.js
+++ src/core/Process.js
@@ -1,3 +1,38 @@
 export class Process {
+    constructor(
+        id,
+        arrivalTime,
+        burstTime,
+        ioStartTime,
+        ioTime,
+        remainingCpu,
+        remainingIo,
+    ) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.ioStartTime = ioStartTime;
+        this.ioTime = ioTime;
+        this.remainingCpu = remainingCpu;
+        this.remainingIo = remainingIo;
+        this.stateHistory = [];
+        this.hasDoneIo = false;
+    }
 
+    tickCpu() {
+        if (this.remainingCpu > 0)
+            --this.remainingCpu;
+    }
+    tickIo() {
+        if (this.remainingIo > 0)
+            --this.remainingIo;
+        if (this.remainingIo == 0)
+            this.hasDoneIo = true;
+    }
+    recordState(state) {
+        this.stateHistory.push(state);
+    }
+    isCompleted() {
+        return this.remainingCpu == 0;
+    }
 }
